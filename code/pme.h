62ec2216d5989f181cf0470e7c7365f81070787a
Huseyin Kaya <huseyin.kaya@asseco-see.com.tr>
2021-03-05 10:13:30 +0300

    Neighborlist computation for partc2partcl

--- code/pme.h
+++ code/pme.h
@@ -2,6 +2,10 @@
 #include <time.h>
 #include "fftw3.h"
 
+typedef struct Vector {double x, y, z;} Vector;
+typedef struct Matrix {double xx, xy, xz, yx, yy, yz, zx, zy, zz;} Matrix;
+typedef struct Triple {int x, y, z;} Triple;
+
 struct O {
   char *test;
   clock_t time;
@@ -11,11 +15,15 @@ extern struct O o;
 // it is recommended that these values be set with the FF_set methods
 typedef struct FF {
   int N;
+  double *q;
   double A[3][3], Ai[3][3];  // A inverse transpose
   double detA;
   double cutoff;  // abs cutorr
   int orderAcc;
   int topGridDim[3];
+  int *nlist;
+  int nlist_len;
+  int nlist_interaction_count;
   double tolDir;
   fftw_complex *fftw_in;
   fftw_plan forward, backward;
@@ -28,22 +36,26 @@ typedef struct FF {
   double *khat;  // grid2grid stencil; has dimensions topGridDim
   double coeff1, coeff2;  // coeffs of const part of energy
   double time_partcl2partcl;
+  double time_nlist;
+  double time_grid2grid;
+  double time_stencil;
+  double time_anterpolation;
+  double time_interpolation;
 } FF;
-FF *FF_new(void);
+FF *FF_new(int N, double *q, double edges[3][3]);
 void FF_set_cutoff(FF *ff, double cutoff);
 double FF_get_cutoff(FF *ff);
 void FF_set_orderAcc(FF *ff, int orderAcc);
 void FF_set_topGridDim(FF *ff, int topGridDim[3]);
 void FF_set_tolDir(FF *ff, double tolDir);
-void FF_build(FF *ff, int N, double edges[3][3]);
+void FF_build(FF *ff, double (*position)[3]);
 double FF_get_cutoff(FF *ff);
 int FF_get_orderAcc(FF *ff);
 void FF_get_topGridDim(FF *ff, int topGridDim[3]);
 double FF_get_tolDir(FF *ff);
 double FF_get_errEst(FF *ff, int N, double *charge);
-void FF_rebuild(FF *ff, double edges[3][3]);
-double FF_energy(FF *ff, int N, double (*force)[3], double (*position)[3],
-                 double *charge, double *weight);
+void FF_rebuild(FF *ff, double edges[3][3], double (*position)[3]);
+double FF_energy(FF *ff, double (*force)[3], double (*position)[3], double *weight);
   // if weights == NULL, unit weights are assumed; otherwise
   // weights should point to an array of length FF_get_maxLevel(ff) + 1
 void FF_delete(FF *ff);
@@ -51,3 +63,6 @@ void FF_delete(FF *ff);
 double msm4g_tictocmanager(int push);
 void msm4g_tic();
 double msm4g_toc();
+
+Vector prod(Matrix m, Vector v);
+Vector prodT(Matrix m, Vector v);
